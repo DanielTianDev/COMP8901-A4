0526198026e7b931e1023b6203f26e604cf60b94
Daniel Tian <daniel_tian2002@hotmail.com>
2018-11-08 16:16:03 -0800

    added file reading

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace AiAssignment4
 {
@@ -10,6 +10,39 @@ namespace AiAssignment4
     {
         static void Main(string[] args)
         {
+
+            List<string> fileContents = new List<string>();
+
+            ReadFile("test-titanic-fatalities.data", ref fileContents);
+
+            foreach (string f in fileContents) Console.WriteLine(f);
+
+
+        }
+
+        static bool ReadFile(string fileName, ref List<string> output)
+        {
+            string line;
+            string dir = Directory.GetCurrentDirectory() + "\\_data\\" + fileName;
+
+            try
+            {
+                StreamReader file = new System.IO.StreamReader(dir); // Read the file line by line.  
+                List<string> lines = new List<string>();
+
+                while ((line = file.ReadLine()) != null) lines.Add(line);
+                file.Close();
+                output = lines;
+            }
+            catch (Exception e)
+            {
+                if (e.Message != null) Console.WriteLine("Error reading data file: " + e.Message);
+                return false;
+            }
+
+            return true;
         }
     }
+
+
 }
